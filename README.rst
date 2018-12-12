ad984c8c7237dd2e29e395041db1f908977faae9
corinaflorescu <corinaaflorescu@gmail.com>
2018-12-12 16:59:36 -0600

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,29 @@
+PositionRank
+
+
+Usage
+Example on how to use it:
+$PositionRank --input_data data/KDD/docs/ --input_gold data/KDD/docs/
+--input_data : directory with text documents to extract the keyphrases for
+
+--input_gold: directory with text documents containing the annotations for each file in the input data (if you want to evaluate)
+
+The full list of command line options is available with $PositionRank --help
+
+
+Installation
+cd PositionRank
+pip install -r requirements.txt
+python setup.py install
+
+Citing
+If you find PositionRank useful in your research, we ask that you cite the following paper:
+
+@inproceedings{florescu2017positionrank,
+  title={Positionrank: An unsupervised approach to keyphrase extraction from scholarly documents},
+  author={Florescu, Corina and Caragea, Cornelia},
+  booktitle={Proceedings of the 55th Annual Meeting of the Association for Computational Linguistics (Volume 1: Long Papers)},
+  volume={1},
+  pages={1105--1115},
+  year={2017}
+}
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+
+requirements = [
+    'wheel>=0.23.0',
+    'argparse>=1.2.1',
+    'futures>=2.1.6',
+    'six>=1.7.3',
+    'psutil>=2.1.1',
+    'nltk>=3.2.4',
+    'networkx>=1.11'
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='PositionRank',
+    version='1.0',
+    description='PositionRank: An Unsupervised Approach to Keyphrase Extraction from Scholarly Documents',
+    long_description=readme,
+    author='Corina Florescu',
+    author_email='corinaflorescu@my.unt.edu',
+    packages=[
+        'PositionRank',
+    ],
+    entry_points={'console_scripts': ['PositionRank = PositionRank.__main__:main']},
+    package_dir={'PositionRank':
+                 'PositionRank'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="GPLv3",
+    zip_safe=False,
+    keywords='deepwalk',
+    classifiers=[
+
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
